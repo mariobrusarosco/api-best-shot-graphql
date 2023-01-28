72dc66bef6870d9d1828015a149fd056a73805ed
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-01-28 10:46:14 -0300

    (Learnings): Server Setup

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,20 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
-
-const BestShotAPI = require("./datasources/best-shot-api");
+import BestShotAPI from "./datasources/best-shot-api";
 
 const server = new ApolloServer({
-  introspection: true,
   typeDefs,
   resolvers,
-  dataSources: () => {
-    return {
-      BestShotAPI: new BestShotAPI(),
-    };
-  },
-  context: {
-    test: "Lorem ipsum",
-  },
 });
 
-server
-  .listen({ port: process.env.PORT || 4000 })
-  .then(({ url }: { url: string }) => {
-    console.log(`
-    🚀  Server is ready at ${url}
-    📭  Query at https://api-dev-best-shot.herokuapp.com/
-  `);
+const main = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
   });
+
+  console.log(`🚀  Server ready at ${url}`);
+};
+
+main();
